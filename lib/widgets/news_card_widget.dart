bfb4d7deedd078db7b43e92cfeb712fb27735a7c
Amit Banik Surjo <amitsurjo57@gmail.com>
2025-02-05 23:55:50 +0600

    Fantasy Cricket App

--- lib/widgets/news_card_widget.dart
+++ lib/widgets/news_card_widget.dart
@@ -12,45 +12,45 @@ class NewsCardWidget extends StatelessWidget {
       width: double.infinity,
       height: 380,
       child: Card(
-        child: SingleChildScrollView(
-          child: Column(
-            spacing: 8,
-            children: [
-              Container(
-                width: double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                    image: NetworkImage(newsModel.image),
-                    fit: BoxFit.cover,
+        child: Column(
+          spacing: 8,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: NetworkImage(newsModel.image),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                spacing: 8,
+                children: [
+                  Text(
+                    newsModel.headline,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
                   ),
-                ),
+                  Text(
+                    newsModel.content,
+                    style: TextStyle(fontSize: 16),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 4,
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  spacing: 8,
-                  children: [
-                    Text(
-                      newsModel.headline,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      newsModel.content,
-                      style: TextStyle(fontSize: 16),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 4,
-                      textAlign: TextAlign.justify,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
